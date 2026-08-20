a238b174726afdf83cd1b83fc7bbb0468da35f22
zerodesu0 <boutagsifo@gmail.com>
2026-08-20 09:20:25 +0100

    Add DeviceTest website

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll("a[href^='#']").forEach(a=>a.addEventListener("click",e=>{const el=document.querySelector(a.getAttribute("href"));if(el){e.preventDefault();el.scrollIntoView({behavior:"smooth"})}}));
